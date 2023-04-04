c1c3bc103c35ae4aeb204a81a054c07d9b492afb
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-04-04 20:25:04 +0200

    Añadido paquete basicScroll

--- U6_Profundidad/script.js
+++ U6_Profundidad/script.js
@@ -1,5 +1,21 @@
+import * as basicScroll from "basicscroll";
+
 document.querySelectorAll(".book").forEach(function (elemento) {
     elemento.addEventListener("click", function () {
         this.classList.toggle("descubierto");
     });
 });
+
+const instance = basicScroll.create({
+    elem: document.querySelector(".element"),
+    from: "top-bottom",
+    to: "middle-middle",
+    props: {
+        "--opacity": {
+            from: 0.01,
+            to: 0.99,
+        },
+    },
+});
+
+instance.start();
